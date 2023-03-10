fbcdf4e2f293b0a7fbc343809f424b927abfad60
AntonioBriPerez <antoniobirrin@gmail.com>
2023-03-10 22:39:20 +0100

    vm for the plex server

--- portainer.tf
+++ portainer.tf
@@ -1,6 +1,6 @@
-resource "proxmox_vm_qemu" "portainer-server" {
-  count = 1                  # just want 1 for now, set to 0 and apply to destroy VM
-  name  = "portainer-server" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+resource "proxmox_vm_qemu" "plex-server" {
+  count = 1             # just want 1 for now, set to 0 and apply to destroy VM
+  name  = "plex-server" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
   target_node = "torre-linux"
   # another variable with contents "ubuntu-2004-cloudinit-template"
