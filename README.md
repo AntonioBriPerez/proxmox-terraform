9f2d6f187a1a206c1a5a4c095c31be04e0bec219
AntonioBriPerez <antoniobirrin@gmail.com>
2023-03-12 18:39:22 +0100

    Added readme, pending to finish it

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# proxmox-terraform
+# Deploying VM's via Terraform
+Thanks to the VM Template deployed via Packer as I have explained [in the Packer repository](https://github.com/AntonioBriPerez/packer-proxmox-templates) it is quite simple to deploy VM's via terraform. 
+It is very importante to notice that the Terraform Proxmox plugin crash when an HDD disk is attached to VM to we have to dettach it manually from the proxmox gui before doing anything in proxmox. 
+
+This repository must containa  file called credentials.auto.tfvars with the following content: 
+```hcl
+proxmox_api_url          = "https://0.0.0.0:8006/api2/json" # Your Proxmox IP Address
+proxmox_api_token_id     = "user@pam!terraform"                   # API Token ID
+proxmox_api_token_secret = "token"
+```
+Chaning this values for whatever it takes in your case. 
+
+## Terraform VM's files
+All 3 current machines: samba, plex and wireguard has the same exact configuration. The code must be refactored but it will be done in the future. 
+
+```hcl
+
+
+```
--- plex-server.tf
+++ plex-server.tf
@@ -1,21 +1,16 @@
 resource "proxmox_vm_qemu" "plex-server" {
-  count = 1             # just want 1 for now, set to 0 and apply to destroy VM
-  name  = "plex-server" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
-  # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
+  count       = 1
+  name        = "plex-server"
   target_node = "torre-linux"
-  # another variable with contents "ubuntu-2004-cloudinit-template"
-  clone = "ubuntu-server-focal-test-1"
-  # basic VM settings here. agent refers to guest agent
-  agent = 1
-  # pxe     = true
-  vmid    = "101"
-  os_type = "cloud-init"
-  # boot    = "order=net0;scsi0"
-  cores   = 2
-  sockets = 1
-  cpu     = "host"
-  memory  = 8192
-  scsihw  = "virtio-scsi-pci"
+  clone       = "ubuntu-server-focal-test-1"
+  agent       = 1
+  vmid        = "101"
+  os_type     = "cloud-init"
+  cores       = 2
+  sockets     = 1
+  cpu         = "host"
+  memory      = 8192
+  scsihw      = "virtio-scsi-pci"
 
   # bootdisk = "scsi0"
   # disk {
@@ -34,7 +29,6 @@ resource "proxmox_vm_qemu" "plex-server" {
     macaddr = "a2:b4:17:8e:39:a4"
 
   }
-  # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
   lifecycle {
     ignore_changes = [
       network,
